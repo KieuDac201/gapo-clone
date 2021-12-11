9110a130e124f929a0e3964bf493205f7ec11627
KieuDac <kieudac18082001@gmail.com>
2021-12-11 19:03:14 +0700

    done header

--- src/context/UserProvider.js
+++ src/context/UserProvider.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useLayoutEffect } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
@@ -8,7 +8,7 @@ export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const auth = getAuth();
   const navigate = useNavigate();
-  useEffect(() => {
+  useLayoutEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
