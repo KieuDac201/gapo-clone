7b16c8d71cd5a2341a2a9c06bce17b6a5d1400d7
KieuDac <kieudac18082001@gmail.com>
2022-02-07 10:20:11 +0700

    fix like function

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,85 +1,127 @@
-import { GiEarthAsiaOceania } from 'react-icons/gi'
-import { HiOutlineDotsHorizontal } from 'react-icons/hi'
-import { BiSend, BiComment, BiShare } from 'react-icons/bi'
-import { AiOutlineLike, AiFillLike } from 'react-icons/ai'
-import { doc, updateDoc } from 'firebase/firestore'
-import { FacebookShareButton } from 'react-share'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { GiEarthAsiaOceania } from "react-icons/gi";
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { BiSend, BiComment, BiShare } from "react-icons/bi";
+import { AiOutlineLike, AiFillLike } from "react-icons/ai";
+import { doc, updateDoc, setDoc } from "firebase/firestore";
+import { FacebookShareButton } from "react-share";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-
-import { Action, Actions, Caption, Cmt, CommentList, Image, LikeAndCmt, Top, Wrapper } from './styled'
-import { likeReact } from '../../images'
-import { v4 as uuidv4 } from 'uuid';
-import { db } from '../../firebase/config'
-import Comment from '../Comment/Comment'
-import { timeSince } from '../../utils'
-import { useUserContext } from '../../context/UserProvider'
-import ModalImg from '../ModalImg/ModalImg'
+import {
+  Action,
+  Actions,
+  Caption,
+  Cmt,
+  CommentList,
+  Image,
+  LikeAndCmt,
+  Top,
+  Wrapper,
+} from "./styled";
+import { likeReact } from "../../images";
+import { v4 as uuidv4 } from "uuid";
+import { db } from "../../firebase/config";
+import Comment from "../Comment/Comment";
+import { timeSince } from "../../utils";
+import { useUserContext } from "../../context/UserProvider";
+import ModalImg from "../ModalImg/ModalImg";
 
 const Post = ({ post }) => {
-  const { nameAuthor, imgAuthor, image, caption, comments, time, like, id, uid } = post
-  const user = useUserContext()
-  const [showMoreCmt, setShowMoreCmt] = useState(1)
-  const [comment, setComment] = useState('')
-  const [commentList, setCommentList] = useState(comments)
-  const [likeList, setLikeList] = useState(like)
-  const [isOpenModal, setIsOpenModal] = useState(false)
-
+  const {
+    nameAuthor,
+    imgAuthor,
+    image,
+    caption,
+    comments,
+    time,
+    like,
+    id,
+    uid,
+  } = post;
+  const user = useUserContext();
+  const [showMoreCmt, setShowMoreCmt] = useState(1);
+  const [comment, setComment] = useState("");
+  const [commentList, setCommentList] = useState(comments);
+  const [likeList, setLikeList] = useState(like);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  console.log(likeList);
 
   const handleLike = async () => {
     const postRef = doc(db, "posts", id);
-    if (likeList[id]) {
-      delete likeList[id]
+    if (likeList[uid]) {
+      delete likeList[uid];
     } else {
-      likeList[id] = 'like'
+      likeList[uid] = "like";
     }
-    setLikeList({ ...likeList })
+    setLikeList({ ...likeList });
     await updateDoc(postRef, {
-      like: likeList
+      like: likeList,
     });
-  }
+  };
 
   const handleComment = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const postRef = doc(db, "posts", id);
-    const data = [...commentList, { id: uuidv4(), time: Date.now(), uid: user.uid, name: user.displayName, img: user.photoURL, comment }]
-    setCommentList(data)
-    setComment('')
+    const data = [
+      ...commentList,
+      {
+        id: uuidv4(),
+        time: Date.now(),
+        uid: user.uid,
+        name: user.displayName,
+        img: user.photoURL,
+        comment,
+      },
+    ];
+    setCommentList(data);
+    setComment("");
     await updateDoc(postRef, {
-      comments: data
+      comments: data,
     });
-  }
+  };
 
   const handleCmtMore = () => {
-    return commentList.length > 1 && showMoreCmt == 1 ?
-      <div onClick={() => setShowMoreCmt(commentList.length)} className='btn-show'>Xem tất cả bình luận</div>
-      :
-      <div onClick={() => setShowMoreCmt(1)} className='btn-show'>Ẩn bớt bình luận</div>
-  }
-
+    return commentList.length > 1 && showMoreCmt == 1 ? (
+      <div
+        onClick={() => setShowMoreCmt(commentList.length)}
+        className="btn-show"
+      >
+        Xem tất cả bình luận
+      </div>
+    ) : (
+      <div onClick={() => setShowMoreCmt(1)} className="btn-show">
+        Ẩn bớt bình luận
+      </div>
+    );
+  };
 
   return (
     <Wrapper>
-
-      <ModalImg isOpenModal={isOpenModal} setIsOpenModal={setIsOpenModal} img={image} />
+      <ModalImg
+        isOpenModal={isOpenModal}
+        setIsOpenModal={setIsOpenModal}
+        img={image}
+      />
       <Top>
         <Link to={`/personal/${uid}`}>
-          <img src={imgAuthor} alt="" className='logo' />
+          <img src={imgAuthor} alt="" className="logo" />
         </Link>
         <div className="nameAndTime">
           <h5>{nameAuthor}</h5>
-          <p>{timeSince(time)}<GiEarthAsiaOceania /></p>
+          <p>
+            {timeSince(time)}
+            <GiEarthAsiaOceania />
+          </p>
         </div>
-        <div className='more'>
+        <div className="more">
           <HiOutlineDotsHorizontal />
         </div>
-      </Top >
+      </Top>
       <Caption>{caption}</Caption>
-      {< Image src={image} onClick={() => setIsOpenModal(true)} />}
+      {<Image src={image} onClick={() => setIsOpenModal(true)} />}
       <LikeAndCmt>
-        <div className='like'>
-          <div className='icon'>
+        <div className="like">
+          <div className="icon">
             <img src={likeReact} alt="" />
           </div>
           <p>{Object.keys(likeList).length}</p>
@@ -87,44 +129,66 @@ const Post = ({ post }) => {
         <p>{commentList.length} bình luận</p>
       </LikeAndCmt>
       <Actions>
-        <Action className={likeList[id] ? 'like active' : 'like'} onClick={handleLike}>
-          {likeList[id] ? <AiFillLike /> : <AiOutlineLike />}
+        <Action
+          className={likeList[uid] ? "like active" : "like"}
+          onClick={handleLike}
+        >
+          {likeList[uid] ? <AiFillLike /> : <AiOutlineLike />}
           Thích
         </Action>
         <Action>
           <BiComment />
           Bình luận
         </Action>
-        <FacebookShareButton url="https://thuyeudac.xyz" quote="Hello everyone" hashtag="#kieudac #frontend #react">
+        <FacebookShareButton
+          url="https://thuyeudac.xyz"
+          quote="Hello everyone"
+          hashtag="#kieudac #frontend #react"
+        >
           <Action>
             <BiShare />
             Chia sẻ
           </Action>
         </FacebookShareButton>
       </Actions>
-      {
-        commentList.length !== 0 && <CommentList>
-
-          {commentList.slice(0, showMoreCmt).map(({ img, id, name, comment, time }) => {
-            return <Comment key={id} img={img} name={name} comment={comment} time={time} />
-          })}
-          {
-            commentList.length !== 1 && handleCmtMore()
-          }
+      {commentList.length !== 0 && (
+        <CommentList>
+          {commentList
+            .slice(0, showMoreCmt)
+            .map(({ img, id, name, comment, time }) => {
+              return (
+                <Comment
+                  key={id}
+                  img={img}
+                  name={name}
+                  comment={comment}
+                  time={time}
+                />
+              );
+            })}
+          {commentList.length !== 1 && handleCmtMore()}
         </CommentList>
-      }
+      )}
       <Cmt>
         <img src={user?.photoURL} alt="image" />
         <div>
           <form onSubmit={handleComment}>
-            <input type="text" placeholder='Viết bình luận' value={comment} onChange={(e) => setComment(e.target.value)} />
+            <input
+              type="text"
+              placeholder="Viết bình luận"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
           </form>
-          {comment && <span onClick={handleComment}><BiSend /></span>}
+          {comment && (
+            <span onClick={handleComment}>
+              <BiSend />
+            </span>
+          )}
         </div>
       </Cmt>
-    </Wrapper >
-  )
-}
+    </Wrapper>
+  );
+};
 
-export default Post
-
+export default Post;
