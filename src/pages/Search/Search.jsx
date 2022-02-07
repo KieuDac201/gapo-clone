8a7695e8778aec4e0333957ecfe6753de1ff34a5
KieuDac <kieudac18082001@gmail.com>
2022-02-07 10:34:40 +0700

    add click to personal page

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,56 +1,68 @@
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import styled from 'styled-components'
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import styled from "styled-components";
 
-import Button from '../../components/Button/Button'
-import Layout from '../../components/Layout/Layout';
-import { getAllUser } from '../../services';
+import Button from "../../components/Button/Button";
+import Layout from "../../components/Layout/Layout";
+import { getAllUser } from "../../services";
 
 const Search = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [userSearch, setUserSearch] = useState([])
-  const querySeacrch = searchParams.get('q').toLowerCase()
+  const [userSearch, setUserSearch] = useState([]);
+  const querySeacrch = searchParams.get("q").toLowerCase();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    getAllUser().then(users => {
-      const userList = users.filter(user => user.displayName.toLowerCase().includes(querySeacrch))
-      setUserSearch(userList)
-    })
-  }, [querySeacrch])
+    getAllUser().then((users) => {
+      const userList = users.filter((user) =>
+        user.displayName.toLowerCase().includes(querySeacrch)
+      );
+      setUserSearch(userList);
+    });
+  }, [querySeacrch]);
 
-  console.log(userSearch)
+  const changeToPersonal = (uid) => {
+    navigate(`/personal/${uid}`);
+  };
+
+  console.log(userSearch);
   return (
     <Layout>
       <Wrapper>
-        {
-          userSearch.length ? userSearch.map(({ displayName, photoURL, uid }) => (
+        {userSearch.length ? (
+          userSearch.map(({ displayName, photoURL, uid }) => (
             <SearchItem key={uid}>
-              <img src={photoURL} alt="picture profile" />
-              <h4>{displayName}</h4>
+              <img
+                src={photoURL}
+                alt="picture profile"
+                onClick={() => changeToPersonal(uid)}
+              />
+              <h4 onClick={() => changeToPersonal(uid)}>{displayName}</h4>
               <span>
                 <Button dark>Add freind</Button>
               </span>
             </SearchItem>
-          )) : <h3>Khong tim thay</h3>
-        }
+          ))
+        ) : (
+          <h3>Khong tim thay</h3>
+        )}
       </Wrapper>
     </Layout>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
 
 const Wrapper = styled.div`
-
   max-width: 450px;
-  width: 100%;  
+  width: 100%;
   position: relative;
   top: 90px;
   left: 50%;
   transform: translateX(-50%);
   height: 4300px;
   padding: 20px;
-`
+`;
 
 const SearchItem = styled.div`
   display: flex;
@@ -58,13 +70,12 @@ const SearchItem = styled.div`
   margin-bottom: 20px;
   justify-content: space-between;
 
-  img{
+  img {
     width: 40px;
     height: 40px;
     border-radius: 50%;
   }
 
-  span{
-
+  span {
   }
-`
+`;
